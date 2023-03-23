e1b1866df155973ca98bc59ebc4fd73170fa2dd2
Swagat <swagatjena0810@gmail.com>
2023-03-24 00:57:37 +0530

    branch testing

--- src/main/java/com/mongo/example/MongodbexampleApplication.java
+++ src/main/java/com/mongo/example/MongodbexampleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MongodbexampleApplication {
 
 	public static void main(String[] args) {
+		System.out.println("sucessful branch tested");
 		SpringApplication.run(MongodbexampleApplication.class, args);
 	}
 
